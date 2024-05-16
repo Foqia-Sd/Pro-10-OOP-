dee0451777615644b6ce3dabd8a6bdf2c6d58321
Foqia-Sd <160354822+Foqia-Sd@users.noreply.github.com>
2024-05-16 23:38:42 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+import inquirer from "inquirer";
+class Student {
+    name;
+    constructor(n) {
+        this.name = n;
+    }
+}
+class Person {
+    students = [];
+    addStudent(obj) {
+        this.students.push(obj);
+    }
+}
+const persons = new Person();
+const programStart = async (persons) => {
+    do {
+        console.log("Welcome!");
+        const ans = await inquirer.prompt({
+            name: "select",
+            type: "list",
+            message: "Whom would you like to interact with?",
+            choices: ["staff", "student", "exit"]
+        });
+        if (ans.select == "staff") {
+            console.log("You like to visit staff room. What is your query?");
+        }
+        else if (ans.select == "student") {
+            const ans = await inquirer.prompt({
+                name: "student",
+                type: "input",
+                message: "Enter student name with whom you want to interact:"
+            });
+            const student = persons.students.find(val => val.name == ans.student);
+            if (!student) {
+                const name = new Student(ans.student);
+                persons.addStudent(name);
+                console.log(`Hello i am ${name.name}. Nice to meet you!`);
+                console.log("New student added");
+                console.log("Current students list:");
+                console.log(persons.students);
+            }
+            else {
+                console.log(`Hello i am ${student.name}. Nice to see you again!`);
+                console.log("Exiting student list:");
+                console.log(persons.students);
+            }
+        }
+        else {
+            ans.exit == "exit";
+            console.log("Exiting the program...");
+            process.exit();
+        }
+    } while (true);
+};
+programStart(persons);
